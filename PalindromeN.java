525130768982f524d90bdebc0faaa456368afe72
Bipana Koirala <bipanakoirala@Bipanas-MacBook-Pro.local>
2024-02-12 09:48:06 -0500

    added palindrome fle

---
+++ PalindromeN.java
@@ -0,0 +1,22 @@
+package FebA;
+
+public class PalindromeN {
+	public static boolean main(String[] args) {
+		String a="kamak";
+		String result="";
+		
+		int last =a.length()-1;
+		int first =0;
+		while(last!=first)
+		{
+			return false;
+			
+		}
+		return true;
+		
+		
+		
+		
+	}
+
+}
